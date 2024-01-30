5366100063baf36bfceca08d13d46deaab49bf78
GraphicJaffer <farhanjaffer2002@gmail.com>
2024-01-30 09:35:14 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+//always remember to make the class name start with a capital letter
+public class Demo {
+    //we use static below becauase when we create a class we need to create its object 
+    //to not create its object as it is the starting point we use static 
+    public static void main(String[] args) {
+        //String[] args is the command line arguments whatever you pass in the terminal while running will be stored their
+        System.out.println("Farhan Jaffer 2105714");
+    }
+}
